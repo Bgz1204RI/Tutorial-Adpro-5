e913ebc2109c2ebb3b269cdbf5a3101072333853
Bgz1204RI <bagaszharif24@gmail.com>
2026-03-31 18:25:19 +0700

    Fix Typos

--- src/model/subscriber.rs
+++ src/model/subscriber.rs
@@ -2,7 +2,7 @@ use rocket::serde::{Deserialize, Serialize};
 use rocket::log;
 use rocket::serde::json::to_string;
 use rocket::tokio;
-use bambangshop::REQUEST_CLIENT;
+use bambangshop::REQWEST_CLIENT;
 use crate::model::notification::Notification;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -15,12 +15,12 @@ pub struct Subscriber {
 impl Subscriber {
     #[tokio::main]
     pub async fn update(&self, payload: Notification) {
-        REQUEST_CLIENT
+        REQWEST_CLIENT
             .post(&self.url)
             .header("Content-Type", "JSON")
             .body(to_string(&payload).unwrap())
             .send().await.ok();
-        log::warn!("Sent {} notification of: [{}] {}, to: {}",
+        log::warn_!("Sent {} notification of: [{}] {}, to: {}",
             payload.status, payload.product_type, payload.product_title, self.url);
     }
 }
